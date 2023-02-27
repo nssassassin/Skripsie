569b0e0b9c9387428af0ca5951ab6891a88916b6
nssassassin <willierossouw001@gmail.com>
2023-02-27 14:29:16 +0200

    Initial connection paramaters for wifi, confirmed

--- uControllerCode/SatelliteStation/src/main.cpp
+++ uControllerCode/SatelliteStation/src/main.cpp
@@ -11,12 +11,38 @@ const char* serverpassword = "testMyWifi";
 int timePeriod = 1000;
 unsigned long currentTime = 0;
 
+//Declare functions
+void onCloseContact();
 
 
 void setup() {
   // put your setup code here, to run once:
+  onCloseContact();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  currentTime = millis();
+      while(millis() < currentTime + timePeriod){
+        //wait approx. [period] ms
+    }
+    if(WiFi.status() == WL_CONNECTED){
+        Serial.println("Connected");
+    }
+  
 }
+
+void onCloseContact() {
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(serverssid, serverpassword);
+
+        while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+}
+
