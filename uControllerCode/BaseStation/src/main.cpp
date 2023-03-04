584081f1f85c7a6781b3d9fa07a52590a4acdd6c
nssassassin <willierossouw001@gmail.com>
2023-03-04 21:02:04 +0200

    Add SD and wifi to sensor boards

--- uControllerCode/BaseStation/src/main.cpp
+++ uControllerCode/BaseStation/src/main.cpp
@@ -1,36 +1,93 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <SPI.h>
+#include <SD.h>
 
-//Declare Variables
+const char* ssid = "router_ssid";
+const char* password = "router_wifi_password";
+const char* apSSID = "base_station_ssid";
+const char* apPassword = "base_station_password";
 
-//Wifi Login
-const char* apSsid     = "test_Wifi";
-const char* apPassword = "testMyWifi";
+const int chipSelect = 5; // change this to the chip select pin of your SD card reader
+const uint16_t port = 8888;
 
-//Timer variables
-int timePeriod = 1000;
-unsigned long currentTime = 0;
+WebServer server(port);
+IPAddress Ip(192, 168, 2, 100);
+IPAddress NMask(255, 255, 255, 0);
 
+void setup() {
+  Serial.begin(115200);
 
-
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println("\n[*] Creating AP");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(apSsid, apPassword);
+  // Configure Wi-Fi hotspot
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(apSSID, apPassword);
+  WiFi.softAPConfig(Ip, Ip, NMask);
+  Serial.println("Wi-Fi hotspot started");
     Serial.print("[+] AP Created with IP Gateway ");
-    Serial.println(WiFi.softAPIP());
+    Serial.println(WiFi.softAPIP()); 
+
+  //Only wait for this connection to check server later.
+  // Connect to Wi-Fi network
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to Wi-Fi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to Wi-Fi");
+  
+
+
+  // Initialize SD card
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("Card initialized.");
+
+  // Route for receiving data
+  server.on("/", HTTP_POST, [](){
+    String data = server.arg("data");
+    Serial.println(data);
+
+    // Open CSV file for writing
+    File file = SD.open("data.csv", FILE_WRITE);
+    if (!file) {
+      Serial.println("File open failed");
+      server.send(500, "text/plain", "File open failed");
+      return;
+    }
+
+    // Write data to file
+    file.println(data);
+    file.close();
+    
+    server.send(200, "text/plain", "OK");
+  });
+
+  server.begin();
+  Serial.println("Server started");
 }
 
 void loop() {
-
-currentTime = millis();
-   
-  Serial.println("Salig");
-   
-    while(millis() < currentTime + timePeriod){
-        //wait approx. [period] ms
+  // Check if Wi-Fi network is still connected
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wi-Fi network disconnected");
+    
+    // Attempt to reconnect to Wi-Fi network
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to Wi-Fi...");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting...");
     }
-
+    Serial.println("Connected to Wi-Fi");
+  }
+  
+  server.handleClient();
 }
+
+//Still need to handle write to server or api, granted access is given to wifi.
--- uControllerCode/SatelliteStation/src/main.cpp
+++ uControllerCode/SatelliteStation/src/main.cpp
@@ -1,48 +1,68 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPI.h>
+#include <SD.h>
 
-//Declare Variables
+const char* ssid = "base_station_ssid";
+const char* password = "base_station_password";
 
-//Wifi Login
-const char* serverssid     = "test_Wifi";
-const char* serverpassword = "testMyWifi";
+const IPAddress serverIP(192, 168, 2, 100); // replace with the IP address of the base station
+const uint16_t serverPort = 8888; // replace with the port number of the base station
 
-//Timer variables
-int timePeriod = 1000;
-unsigned long currentTime = 0;
+WiFiClient client;
 
-//Declare functions
-void onCloseContact();
-
+const int chipSelect = 5; // change this to the chip select pin of your SD card reader
 
 void setup() {
-  // put your setup code here, to run once:
-  onCloseContact();
+  Serial.begin(115200);
+  
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Connect to base station
+  Serial.println("Connecting to base station...");
+  while (!client.connect(serverIP, serverPort)) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to base station");
+
+  // Initialize SD card
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("Card initialized.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  currentTime = millis();
-      while(millis() < currentTime + timePeriod){
-        //wait approx. [period] ms
-    }
-    if(WiFi.status() == WL_CONNECTED){
-        Serial.println("Connected");
-    }
-  
+  // Open CSV file for reading
+  File file = SD.open("data.csv");
+  if (!file) {
+    Serial.println("File not found");
+    return;
+  }
+
+  // Read CSV values and send them to base station
+  while (file.available()) {
+    String line = file.readStringUntil('\n');
+    client.write(line.c_str(), line.length());
+    Serial.println(line);
+  }
+  file.close();
+
+  // Wait for response from base station
+  while (client.available()) {
+    String response = client.readString();
+    Serial.println(response);
+  }
+
+  delay(1000);
 }
-
-void onCloseContact() {
-    WiFi.mode(WIFI_STA); //Optional
-    WiFi.begin(serverssid, serverpassword);
-
-        while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
-
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
-}
-
