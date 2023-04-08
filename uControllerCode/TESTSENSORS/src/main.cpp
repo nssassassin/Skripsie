927dad9d496c4432c2465fcc69055034ae2e95bf
nssassassin <61880673+nssassassin@users.noreply.github.com>
2023-04-08 18:55:22 +0200

    Add files via upload

---
+++ uControllerCode/TESTSENSORS/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+uint8_t SDApin = SDA;//33
+uint8_t SCLpin = SCL;//35
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
